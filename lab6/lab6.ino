a7b1fc7774693f1f82bcc77d2b5e09f5dcc4e8e1
unknown <mp65@rice.edu>
2024-02-20 14:06:31 -0600

    Time data storage flags/variables & thresholds added

--- lab6/lab6.ino
+++ lab6/lab6.ino
@@ -1,6 +1,17 @@
+// initial variables
 const int sensor_pin = 0;
 int pulse_signal = 0;
 float BPM = 0;
+
+// two flags & two variables to store time data
+bool any_peak_detected = false;
+bool first_peak_detected = false;
+unsigned long first_pulse_time = 0;
+unsigned long second_pulse_time = 0;
+
+// thresholds for pulse waveform
+int upper_threshold = 820;
+int lower_threshold = 800;
 
 void setup() {
   // put your setup code here, to run once:
