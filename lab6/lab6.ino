46749b51c0aec3408d97c3bcf796446098d64b15
unknown <mp65@rice.edu>
2024-02-20 14:01:55 -0600

    Visualization of pulse data added

--- lab6/lab6.ino
+++ lab6/lab6.ino
@@ -5,9 +5,16 @@ float BPM = 0;
 void setup() {
   // put your setup code here, to run once:
 
+  // initialize serial communications
+  Serial.begin(9600);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  // collect pulse signal and display in serial monitor
+  pulse_signal = analogRead(sensor_pin);
+  Serial.println(pulse_signal);
 
 }
