4615b315842d4d2a144c04ffd3aff246dd02f0ba
unknown <mp65@rice.edu>
2024-02-26 23:18:34 -0600

    final changes

--- lab6/lab6.ino
+++ lab6/lab6.ino
@@ -8,10 +8,11 @@ bool any_peak_detected = false;
 bool first_peak_detected = false;
 unsigned long first_pulse_time = 0;
 unsigned long second_pulse_time = 0;
+float pulse_period = 0;
 
 // thresholds for pulse waveform
-int upper_threshold = 820;
-int lower_threshold = 800;
+int upper_threshold = 950;
+int lower_threshold = 775;
 
 void setup() {
   // put your setup code here, to run once:
@@ -27,5 +28,29 @@ void loop() {
   // collect pulse signal and display in serial monitor
   pulse_signal = analogRead(sensor_pin);
   Serial.println(pulse_signal);
+
+  // peak detecion if statement
+  delay(50);
+  if (pulse_signal > upper_threshold && any_peak_detected == false) {
+    any_peak_detected = true;
+    // Do something about this peak
+
+    if (first_peak_detected == false) {
+      first_pulse_time = millis();
+      first_peak_detected = true;
+    } else {
+      second_pulse_time = millis();
+      first_peak_detected = false;
+      pulse_period = second_pulse_time - first_pulse_time;
+    }
+
+  }
+  // reset peak detection if drops below lower threshold
+  if (pulse_signal < lower_threshold) {
+    any_peak_detected = false;
+  }
+
+  float pulse = 60000/pulse_period;
+  Serial.print(pulse);
 
 }
